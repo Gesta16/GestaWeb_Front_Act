e8432687f565f1b387d63adc2ea688a9f4c3c332
Marly <rangelmarly@gmail.com>
2024-11-07 16:34:28 -0500

    reestructuracion ruta-gestante

--- src/app/SharedModule/shared.module.ts
+++ src/app/SharedModule/shared.module.ts
@@ -29,6 +29,7 @@ import { Ruta3Component } from './pages/formulario/ruta-3/ruta-3.component';
 import { Ruta4Component } from './pages/formulario/ruta-4/ruta-4.component';
 import { Ruta5Component } from './pages/formulario/ruta-5/ruta-5.component';
 import { Ruta6Component } from './pages/formulario/ruta-6/ruta-6.component';
+import { DahboardComponent } from './pages/dahboard/dahboard.component';
 
 
 
@@ -62,7 +63,8 @@ import { Ruta6Component } from './pages/formulario/ruta-6/ruta-6.component';
     Ruta3Component,
     Ruta4Component,
     Ruta5Component,
-    Ruta6Component
+    Ruta6Component,
+    DahboardComponent
   ],
   imports: [
     CommonModule,
