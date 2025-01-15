3b3ac4f2369dadebbe288f65b9f2a182e6c89fea
Marly <rangelmarly@gmail.com>
2025-01-15 10:20:00 -0500

    cambios

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    //apiUrl: 'http://api.gestawebucc.com/api/'
-    apiUrl: 'http://127.0.0.1:8000/api/'
+    production: true,
+    apiUrl: 'https://api.gestawebucc.com/api/'
+    // apiUrl: 'http://127.0.0.1:8000/api/'
   };
   
