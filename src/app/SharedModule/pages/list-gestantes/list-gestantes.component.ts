0b50dd9dedcb9fd78e2542536ac2dba7f9d667be
Marly <rangelmarly@gmail.com>
2025-01-19 14:26:11 -0500

    quite console logs

--- src/app/SharedModule/pages/list-gestantes/list-gestantes.component.ts
+++ src/app/SharedModule/pages/list-gestantes/list-gestantes.component.ts
@@ -86,7 +86,7 @@ export class ListGestantesComponent {
       const usuario = this.usuarios.find(u => u.id_usuario === usuarioId); // Busca el usuario por id
       if (usuario) {
         usuario.procesosCount = response.numero_de_procesos_gestativos; // Almacena el conteo en el objeto usuario
-        console.log(`Número de procesos gestativos para ${usuarioId}: ${response.numero_de_procesos_gestativos}`);
+        //console.log(`Número de procesos gestativos para ${usuarioId}: ${response.numero_de_procesos_gestativos}`);
       }
     }, error => {
       console.error('Error al contar los procesos gestativos:', error);
@@ -95,7 +95,7 @@ export class ListGestantesComponent {
 
   crearProceso(usuarioId: number) {
     this.usuarioService.crearProcesoGestativo(usuarioId).subscribe(response => {
-      console.log(response.message);
+      //console.log(response.message);
       this.loadUsuarios();
     }, error => {
       console.error('Error al crear el proceso gestativo:', error);
@@ -135,7 +135,7 @@ export class ListGestantesComponent {
   private loadUsuarios(): void {
     this.usuarioService.getUsuarios().subscribe(
       (response: any) => {
-        console.log(response.usuarios);
+        //console.log(response.usuarios);
         this.usuarios = response.usuarios;
         // Contar los procesos gestativos para cada usuario
         this.usuarios.forEach(usuario => {
