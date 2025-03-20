37a9c56b9774ad07a123962e5e070082b2addbe1
Marly <rangelmarly@gmail.com>
2025-03-20 08:42:05 -0500

    Arreglos nombres de laboratorios gestante

--- src/app/Gestantes/pages/examenes/examenes.component.ts
+++ src/app/Gestantes/pages/examenes/examenes.component.ts
@@ -22,48 +22,49 @@ export class ExamenesComponent {
   laboratorioITrimestre: LaboratorioITrimestre;
   laboratorioIITrimestre: LaboratorioIITrimestre;
   laboratorioIIITrimestre: LaboratorioIIITrimestre;
-  
+
 
   examenes1 = [
-    { nombre: 'Hemograma',                              key:'real_hemograma',  completado: false },
-    { nombre: 'Glicemia',                               key:'real_glicemia',  completado: false},
-    { nombre: 'Antigeno de Superficie Hepatitis B',     key:'real_antigenos',  completado: false},
-    { nombre: 'VIH',                                    key:'real_prueb_rapi_vih',          completado: false },
-    { nombre: 'Prueba Treponémica Rápida Para Sífilis', key:'reali_prueb_trepo_rapid_sifilis',      completado: false},
-    { nombre: 'Urocultivo',                             key:'realizo_urocultivo',  completado: false},
-    { nombre: 'Antibiograma',                           key:'realizo_antibiograma',    completado: false},
-    { nombre: 'IG-G Rubeola',                           key:'real_ig_rubeola',    completado: false},
-    { nombre: 'IG-G Toxoplasma',                        key:'real_ig_toxoplasma',    completado: false},
-    { nombre: 'IG-M Toxoplasma',                        key:'real_igm_toxoplasma',    completado: false},
-    { nombre: 'Hemoparasito - Gota Gruesa',             key:'real_hemoparasito',    completado: false},
-    { nombre: 'Prueba De Elisa Antigenos Totales/Crudo En (Zonas Endémicas Chagas)', key:'real_prueb_eliza_anti_total',  completado: false},
-    { nombre: 'Elisa Antigeno Recombinante',                  key:'real_prueb_eliza_anti_recomb', completado: false},
-    { nombre: 'Coombs Indirecto Cuantitativo',                key:'real_prueb_coombis_indi_cuanti', completado: false},
-    { nombre: 'Ecografia Obstetrica Tamizaje de Aneuploidis', key:'real_eco_obste_tamizaje', completado: false},
+    { nombre: 'Hemoclasificacion', key: 'fec_hemoclasificacion', completado: false },
+    { nombre: 'Hemograma', key: 'real_hemograma', completado: false },
+    { nombre: 'Glicemia', key: 'real_glicemia', completado: false },
+    { nombre: 'Antigeno de Superficie Hepatitis B', key: 'real_antigenos', completado: false },
+    { nombre: 'Prueba rápida de VIH', key: 'real_prueb_rapi_vih', completado: false },
+    { nombre: 'Prueba Treponémica Rápida Para Sífilis', key: 'reali_prueb_trepo_rapid_sifilis', completado: false },
+    { nombre: 'Urocultivo', key: 'realizo_urocultivo', completado: false },
+    { nombre: 'Antibiograma', key: 'realizo_antibiograma', completado: false },
+    { nombre: 'IG-G Rubeola', key: 'real_ig_rubeola', completado: false },
+    { nombre: 'IG-G Toxoplasma', key: 'real_ig_toxoplasma', completado: false },
+    { nombre: 'IG-M Toxoplasma', key: 'real_igm_toxoplasma', completado: false },
+    { nombre: 'Hemoparasito - Gota Gruesa', key: 'real_hemoparasito', completado: false },
+    { nombre: 'Prueba De Elisa Antigenos Totales/Crudo En (Zonas Endémicas Chagas)', key: 'real_prueb_eliza_anti_total', completado: false },
+    { nombre: 'Elisa Antigeno Recombinante', key: 'real_prueb_eliza_anti_recomb', completado: false },
+    { nombre: 'Coombs Indirecto Cuantitativo', key: 'real_prueb_coombis_indi_cuanti', completado: false },
+    { nombre: 'Ecografia Obstetrica de translucencia nucal semana 11-14', key: 'real_eco_obste_tamizaje', completado: false },
   ];
 
   examenes2 = [
-    { nombre: 'VIH',                                            key:'reali_prueb_rapi_vih', completado: false },
-    { nombre: 'Prueba Treponémica Rápida Para Sífilis',         key:'real_prueb_trep_rap_sifilis', completado: false},
-    { nombre: 'Prueba De Tolerancia Oral A La Glucosa Basal',   key:'real_prueb_oral', completado: false},
-    { nombre: 'Prueba De Tolerancia Oral A La Glucosa 1 Hora',  key:'real_prueb_oral_1', completado: false},
-    { nombre: 'Prueba De Tolerancia Oral A La Glucosa 2 Horas', key:'real_prueb_oral_2', completado: false},
-    { nombre: 'Reporte De Citologia',                           key:'reali_citologia', completado: false},
-    { nombre: 'IG-M Toxoplasma',                                key:'real_igm_toxoplasma_2', completado: false},
-    { nombre: 'Prueba De Avidez Ig G',                          key:'reali_prueb_avidez_ig_g', completado: false},
-    { nombre: 'Toxoplasmosis IG-A ',                            key:'reali_prueb_toxoplasmosis_ig_a', completado: false},
-    { nombre: 'Hemoparasito - Gota Gruesa',                     key:'reali_prueb_hemoparasito', completado: false},
-    { nombre: 'Coombs Indirecto Cualitativo',                   key:'reali_prueb_coombis_indi_cuanti', completado: false},
-    { nombre: 'Ecografia Obstetrica Detalle Anatomic',          key:'reali_eco_obste_detalle_anato', completado: false},
+    { nombre: 'Prueba rápida de VIH', key: 'reali_prueb_rapi_vih', completado: false },
+    { nombre: 'Prueba Treponémica Rápida Para Sífilis', key: 'real_prueb_trep_rap_sifilis', completado: false },
+    { nombre: 'Prueba De Tolerancia Oral A La Glucosa Basal', key: 'real_prueb_oral', completado: false },
+    { nombre: 'Prueba de tolerancia a la glucosa con carga de 75g', key: 'real_prueb_oral_1', completado: false },
+    // { nombre: 'Prueba De Tolerancia Oral A La Glucosa 2 Horas', key: 'real_prueb_oral_2', completado: false },
+    { nombre: 'Citología cérvico uterina', key: 'reali_citologia', completado: false },
+    { nombre: 'IG-M Toxoplasma', key: 'real_igm_toxoplasma_2', completado: false },
+    { nombre: 'Prueba De Avidez Ig G', key: 'reali_prueb_avidez_ig_g', completado: false },
+    { nombre: 'Toxoplasmosis IG-A ', key: 'reali_prueb_toxoplasmosis_ig_a', completado: false },
+    { nombre: 'Hemoparasito - Gota Gruesa', key: 'reali_prueb_hemoparasito', completado: false },
+    { nombre: 'Coombs Indirecto Cualitativo', key: 'reali_prueb_coombis_indi_cuanti', completado: false },
+    { nombre: 'Ecografia Obstetrica Detalle Anatómico Semana 20-24', key: 'reali_eco_obste_detalle_anato', completado: false },
   ];
 
   examenes3 = [
-    { nombre: 'Hemograma',                              key:'reali_hemograma',                completado: false},
-    { nombre: 'VIH',                                    key:'reali_prueb_rapi_vih_3',         completado: false},
-    { nombre: 'Prueba Treponémica Rápida Para Sífilis', key:'reali_prueb_trepo_rapi_sifilis', completado: false},
-    { nombre: 'IG-M Toxoplasma',                        key:'reali_prueb_igm_toxoplasma',     completado: false},
-    { nombre: 'Cultivo Rectal Y Vaginal',               key:'reali_prueb_culti_rect_vagi',    completado: false},
-    { nombre: 'Perfil Biofisico',                       key:'reali_prueb_perfil_biofisico',   completado: false},
+    { nombre: 'Hemograma', key: 'reali_hemograma', completado: false },
+    { nombre: 'VIH', key: 'reali_prueb_rapi_vih_3', completado: false },
+    { nombre: 'Prueba Treponémica Rápida Para Sífilis', key: 'reali_prueb_trepo_rapi_sifilis', completado: false },
+    { nombre: 'IG-M Toxoplasma', key: 'reali_prueb_igm_toxoplasma', completado: false },
+    { nombre: 'Cultivo recto vaginal para estreptococo del grupo B', key: 'reali_prueb_culti_rect_vagi', completado: false },
+    { nombre: 'Perfil Biofisico', key: 'reali_prueb_perfil_biofisico', completado: false },
   ];
 
   constructor(
@@ -71,7 +72,7 @@ export class ExamenesComponent {
     private authService: AuthService,
     private laboratorioIService: LaboratorioisemestreService,
     private laboratorioIIService: LaboratorioiisemestreService,
-    private laboratorioIIIService: LaboratorioiiisemestreService 
+    private laboratorioIIIService: LaboratorioiiisemestreService
 
   ) { }
 
@@ -99,7 +100,7 @@ export class ExamenesComponent {
       err => {
         console.log(err);
       }
-    ); 
+    );
   }
 
   getLaboratorioII() {
